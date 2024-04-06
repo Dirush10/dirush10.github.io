04f193554b202e4220a8129f9d25885f85365b7b
dinesh-madhavan <dinesh.madhavan10@gmail.com>
2024-04-06 18:20:28 +1100

    fixed script6.js

--- script1.js
+++ script1.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
             physics: [
                 { name: "Dual Nature of Radiation and Matter & Atoms", url: "physics-ch1.html" },
                 { name: "Semi Conductors", url: "physics-ch2.html" },
-                { name: "Physics Formulas 1 ", url: "physics-formula.html" },
+                { name: "Physics Formulas 1", url:"physics-formula.html" },
                 { name: "Physics Formulas 2",url:"physics-formula2.html"},
                 // Add more chapters as needed
             ],
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ],
             biology: [
 
-                {name: "Biology Reproduction", url:"biology-ch1.html"}
+                {name: "Biology Reproduction", url:"biology-ch1.html"},
             ]
             // Add more subjects and their chapters as needed
         };
